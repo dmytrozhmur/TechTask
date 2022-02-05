f705c3f67997dde533bed9282150935cd46d0081
dmitriy <dmytro.zhmur@nure.ua>
2022-02-06 00:24:13 +0200

    Initial commit

---
+++ src/main/java/dbtools/DataBaseHandler.java
@@ -0,0 +1,9 @@
+package dbtools;
+
+public class DataBaseHandler {
+    private static final String HOST = "localhost";
+    private static final String PORT = "5432";
+    private static final String USER = "postgres";
+    private static final String PASS = "3102";
+    private static final String NAME = "staff";
+}
