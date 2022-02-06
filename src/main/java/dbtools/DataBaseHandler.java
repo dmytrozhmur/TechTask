24c447d37d2596a8c86d516dd224764934ca1b6c
dmitriy <dmytro.zhmur@nure.ua>
2022-02-06 19:50:25 +0200

    Added logic to DataBaseHandler class

--- src/main/java/dbtools/DataBaseHandler.java
+++ src/main/java/dbtools/DataBaseHandler.java
@@ -1,9 +1,31 @@
 package dbtools;
 
+import java.sql.*;
+
 public class DataBaseHandler {
-    private static final String HOST = "localhost";
-    private static final String PORT = "5432";
-    private static final String USER = "postgres";
-    private static final String PASS = "3102";
-    private static final String NAME = "staff";
+    private final String dbPass;
+    private final String dbUser;
+    private final String connectionString;
+
+    public DataBaseHandler() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        connectionString = String.format("jdbc:postgresql://%s:%d/%s", "localhost", 5432, "staff");
+        dbUser = "postgres";
+        dbPass = "3102";
+    }
+
+    public ResultSet getAllRecords(String tableName) {
+        ResultSet records = null;
+        try(Connection connection = DriverManager.getConnection(connectionString, dbUser, dbPass)) {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM " + tableName);
+            records = statement.executeQuery();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return records;
+    }
 }
